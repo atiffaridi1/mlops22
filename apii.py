b7d6d1f46c1fbc136d8236190539777723925c50
atiffaridi1 <111214244+atiffaridi1@users.noreply.github.com>
2022-11-14 11:51:41 +0530

    Add files via upload

---
+++ apii.py
@@ -0,0 +1,30 @@
+# Using flask to make an api
+# import necessary libraries and functions
+from flask import Flask, jsonify, request
+import pickle
+# creating a Flask app
+app = Flask(__name__)
+
+
+model_path = "model.pkl"
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+        if(request.method == 'GET'):
+
+                data = "hello world"
+                return jsonify({'data': data})
+
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    image1 = request.json['image1']
+    image2 = request.json['image2']
+    print("done loading")
+    predicted1 = model.predict([image1])
+    predicted2 = model.predict([image2])
+    return {"y_predicted":int(predicted[0])}
